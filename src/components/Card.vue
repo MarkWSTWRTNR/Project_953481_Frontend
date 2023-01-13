1b5cfb83c772ae463c7db5b292e4a758cd12ae29
markwst <Wongsathorn_W@gmail.com>
2023-01-13 16:08:51 +0700

    Search By Title

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+      <div class="event-card">
+        <img :src="anime.main_picture" />
+        <span>{{ anime.title }}</span>
+      </div>
+  </template>
+  <script>
+  export default {
+    name: 'EventCard',
+    props: ['anime']
+  }
+  </script>
+  <style scoped>
+  .event-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39495c;
+    margin-bottom: 18px;
+  }
+  
+  .event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+  }
+  
+  .event-link {
+    color: #2c3e50;
+    text-decoration: none;
+  }
+  </style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <form class="search-box" @submit.prevent="SearchTitle">
+      <input
+        type="search"
+        class="search-field"
+        placeholder="Search for title:"
+        v-model="searchQuery"
+      />
+    </form>
+    <div>
+      <Card v-for="anime in animeList"
+      :key="anime.mal_id"
+      :anime="anime" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import Card from '../components/Card.vue'
 export default {
+
   name: "HomeView",
   components: {
-    HelloWorld,
+    Card
   },
+  data() {
+    return {
+      searchQuery: "",
+      animeList: []
+    }
+  },
+  methods:{
+    async SearchTitle() {
+      const response = await fetch (`http://localhost:5000/searchbytitle?query=${this.searchQuery}`)     
+      this.animeList = await response.json();
+      console.log(this.animeList)
+    }
+  }
 };
 </script>
