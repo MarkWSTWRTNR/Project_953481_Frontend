f5d33d5ed65bc79e42193be34a9967bc39d7341f
markwst <Wongsathorn_W@gmail.com>
2023-01-15 14:57:06 +0700

    fix error text (Not finish yet)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="events">
     <form class="search-box" v-if="byTitle" @submit.prevent="SearchTitle">
       <input
@@ -24,6 +24,43 @@
     </div>
     
   </div>
+</template> -->
+<template>
+  <div class="events">
+    <form class="search-box" v-if="byTitle" @submit.prevent="SearchTitle">
+      <input
+        type="search"
+        class="search-field"
+        placeholder="Search for title:"
+        v-model="searchQuery"
+      />
+    </form>
+    <form class="search-box" v-else @submit.prevent="SearchDescription">
+      <input
+        type="search"
+        class="search-field"
+        placeholder="Search for description:"
+        v-model="searchQuery"
+      />
+    </form>
+    <button @click="this.byTitle = !this.byTitle">Switch search type</button>
+  </div>
+  <div>
+    <div v-if="animeList.similar == null" class="events-cards">
+      <Card
+        v-for="anime in animeList"
+        :key="anime.mal_id"
+        :anime="anime"
+      />
+    </div>
+    <div v-else class="not-found">
+      <h3>
+        Did you mean:
+        <i @click.prevent="SearchBySimilar">{{ animeList.similar }}</i>
+      </h3>
+      <h1>404 not found..</h1>
+    </div>
+  </div>
 </template>
 <script>
 // @ is an alias to /src
@@ -46,11 +83,29 @@ export default {
       const response = await fetch (`http://localhost:5000/searchbytitle?query=${this.searchQuery}`)     
       this.animeList = await response.json();
       console.log(this.animeList)
+      this.searchQuery = "";
     },
     async SearchDescription() {
       const response = await fetch (`http://localhost:5000/searchbydescription?query=${this.searchQuery}`)     
       this.animeList = await response.json();
       console.log(this.animeList)
+      this.searchQuery = "";
+    },
+    SearchBySimilar() {
+      this.searchQuery = this.animeList.similar;
+      if (this.searchBy == "title") {
+        this.SearchTitle();
+      } else {
+        this.SearchDescription();
+      }
+    },
+    async ByTitle() {
+      this.searchQuery = "";
+      this.searchBy = "title";
+    },
+    async ByDescription() {
+      this.searchQuery = "";
+      this.searchBy = "description";
     },
   }
 };
